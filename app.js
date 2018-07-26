228a149c51aa31ce2665e1d5dd76013b834c8565
hf04097@st.habib.edu.pk <Serina_7904>
2018-07-26 16:24:49 +0500

    Final Destiny

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 //Helper Function To Deal with DOM options and Return Select element with options of the iterable parameter
-function createOptions(array){
-    let select = document.createElement('select')
-    for(i=0;i < array.length; i++){
-        let option = document.createElement('option')
-        let text = document.createTextNode(array[i])
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
+
+var courseAddBtn = document.getElementById('addCourseBtn');
+var removeCourseBtn = document.getElementById('removeCourseBtn');
+var calculateBtn = document.getElementById('calculateBtn');
+
+
+function createOptions(array) {
+    var select = document.createElement('select')
+    for (var i = 0; i < array.length; i++) {
+        var option = document.createElement('option')
+        var text = document.createTextNode(array[i])
         option.appendChild(text)
         select.appendChild(option)
     }
     return select
 }
 
-
-let semester = {
-    gradeValues: ["A+","A","A-","B+","B","B-","C+","C","C-","F"],
-    creditValues: [1,2,3,4],
-    gpaValues : {"A+":4,"A":4,"A-":3.67,"B+":3.33,"B":3,"B-":2.67,"C+":2.33,
-    "C":2,"C-":1.67,"F":0},
+var semester = {
+    gradeValues: ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "F"],
+    creditValues: [1, 2, 3, 4],
+    gpaValues: {
+        "A+": 4, "A": 4, "A-": 3.67, "B+": 3.33, "B": 3, "B-": 2.67, "C+": 2.33,
+        "C": 2, "C-": 1.67, "F": 0
+    },
     //Method To Add Another Row/Course
-    add: ()=>{
-        let tbody = document.getElementById('tbody')
-        let tr = document.createElement('tr')
+    add: function () {
+        var tbody = document.getElementById('tbody')
+        var tr = document.createElement('tr')
         tr.className = "rows"
-        for(var i = 0;i < 3; i++){
-            let td = document.createElement('td')
-            let input = document.createElement('input')
-            let select
-            switch(i){
+        for (var i = 0; i < 3; i++) {
+            var td = document.createElement('td')
+            var input = document.createElement('input')
+            var select
+            switch (i) {
                 case 0:
                     input.className = "courses"
                     td.appendChild(input)
@@ -48,26 +66,30 @@ let semester = {
         document.getElementById('gpaContainer').className = ""
     },
     //Method To remove the last row
-    remove: ()=>{
-        let rows = document.getElementsByClassName('rows')
-        if(rows.length > 1){
-            rows[rows.length-1].remove()
+    remove: function () {
+        var rows = document.getElementsByClassName('rows')
+        if (rows.length > 1) {
+            rows[rows.length - 1].remove();
         }
         document.getElementById('gpaContainer').innerHTML = ""
         document.getElementById('gpaContainer').className = ""
     },
     //Method To calculate GPA
-    calculate: ()=>{
-        let credits = document.getElementsByClassName('credits')
-        let grades = document.getElementsByClassName('grades')
-        let gradePoints = 0
-        let sumOfCredits = 0
-        for(var i = 0; i < grades.length;i++){
-            gradePoints += semester.gpaValues[grades[i].value]*credits[i].value
+    calculate: function () {
+        var credits = document.getElementsByClassName('credits')
+        var grades = document.getElementsByClassName('grades')
+        var gradePoints = 0
+        var sumOfCredits = 0
+        for (var i = 0; i < grades.length; i++) {
+            gradePoints += semester.gpaValues[grades[i].value] * credits[i].value
             sumOfCredits += parseInt(credits[i].value)
         }
-        let gpa = (gradePoints/sumOfCredits).toFixed(2)
+        var gpa = (gradePoints / sumOfCredits).toFixed(2)
         document.getElementById('gpaContainer').innerHTML = "Your Gpa: " + gpa
         document.getElementById('gpaContainer').className = "gpaContainer"
     }
 }
+
+courseAddBtn.onclick = semester.add;
+removeCourseBtn.onclick = semester.remove;
+calculateBtn.onclick = semester.calculate;
